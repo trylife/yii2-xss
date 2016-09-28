90334cd8d426571fe11291c9e78ea61d10fcb534
trylife <trylife@qq.com>
2016-09-28 15:57:30 +0800

    add asset

---
+++ XssAsset.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace trylife\yii2Xss;
+
+class XssAsset extends \yii\web\AssetBundle
+{
+    public $sourcePath = '@bower/xss/';
+
+    public $js = [
+        'dist/xss.min.js';
+    ];
+}
